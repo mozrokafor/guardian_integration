d627562f3eaa6f7cd1d87f8085b363dd32436bfe
mozrokafor <94625718+mozrokafor@users.noreply.github.com>
2022-04-27 22:45:28 -0400

    redirects run

--- playwright.config.js
+++ playwright.config.js
@@ -64,12 +64,12 @@ const config = {
         ...devices['Desktop Firefox'],
       },
     },
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-      },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: {
+    //     ...devices['Desktop Safari'],
+    //   },
+    // },
 
     /* Test against mobile viewports. */
     // {
