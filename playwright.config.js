d11fc7b44b1ea43a7465a770e1e963cecfe37d16
mozrokafor <94625718+mozrokafor@users.noreply.github.com>
2022-05-24 08:02:12 -0400

    wip 14

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,7 @@ require('dotenv').config()
  */
 const config = {
   /* Add any setup files here. */
-  globalSetup: require.resolve('./global-setup.js'),
+  // globalSetup: require.resolve('./global-setup.js'),
 
   /* Max time in milliseconds the whole test suite can to prevent CI breaking. */
   globalTimeout: process.env.CI ? 60 * 60 * 1000 : undefined,
